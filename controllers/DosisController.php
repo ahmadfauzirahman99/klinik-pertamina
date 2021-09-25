2e732c771454a63e2b5e8ec694b6ba49640ffb31
hendra yodhi <mas.hendrayodhi@gmail.com>
2021-09-25 16:43:24 +0700

    update : menu master dosis

--- controllers/DosisController.php
+++ controllers/DosisController.php
@@ -8,6 +8,7 @@ use app\models\DosisSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\helpers\Url;
 
 /**
  * DosisController implements the CRUD actions for Dosis model.
@@ -66,10 +67,19 @@ class DosisController extends Controller
     {
         $model = new Dosis();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id_dosis]);
-        }
+        // if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        //     return $this->redirect(['view', 'id' => $model->id_dosis]);
+        // }
 
+        if ($model->load(Yii::$app->request->post())) {
+
+            if ($model->save()) {
+                Yii::$app->session->setFlash('success', '<span style="color:#fff">Berhasil menyimpan data Dosis. <b>' . $model->id_dosis . ' : '. $model->nama_dosis . '</b> </span>&nbsp; <a style="color:#fff"class="btn bg-gradient-secondary" href="' . Url::to(['/doksis/update', 'id' => $model->id_dosis]) . '">Ubah <i class="fas fa-edit fa-md"></i></a>');
+            } else {
+                Yii::$app->session->setFlash('error', 'Gagal menyimpan data Dosis. <pre>' . json_encode($model->errors) . '</pre>');
+            }
+            return $this->redirect('index');
+        }
         return $this->render('create', [
             'model' => $model,
         ]);
@@ -86,10 +96,19 @@ class DosisController extends Controller
     {
         $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id_dosis]);
-        }
+        // if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        //     return $this->redirect(['view', 'id' => $model->id_dosis]);
+        // }
 
+        if ($model->load(Yii::$app->request->post())) {
+
+            if ($model->save()) {
+                Yii::$app->session->setFlash('success', '<span style="color:#fff">Berhasil mengubah data Dosis. <b>' . $model->id_dosis . ' : '. $model->nama_dosis . '</b> </span>&nbsp; <a style="color:#fff"class="btn bg-gradient-secondary" href="' . Url::to(['/doksis/update', 'id' => $model->id_dosis]) . '">Ubah <i class="fas fa-edit fa-md"></i></a>');
+            } else {
+                Yii::$app->session->setFlash('error', 'Gagal mengubah data Dosis. <pre>' . json_encode($model->errors) . '</pre>');
+            }
+            return $this->redirect('index');
+        }
         return $this->render('update', [
             'model' => $model,
         ]);
--- views/dosis/_form.php
+++ views/dosis/_form.php
@@ -9,17 +9,33 @@ use yii\bootstrap4\ActiveForm;
 ?>
 
 <div class="dosis-form">
+    <div class="card">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin([
+            'layout' => 'horizontal',
+            'fieldConfig' => [
+                'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                'horizontalCssClasses' => [
+                    'label' => 'col-sm-2 col-form-label-sm',
+                    'wrapper' => 'col-sm-10',
+                    'error' => '',
+                    'hint' => '',
+                ],
+            ],
+        ]); ?>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <?= $form->field($model, 'nama_dosis')->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($model, 'keterangan')->textarea(['rows' => 6]) ?>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            <?= Html::a('Kembali', Yii::$app->request->referrer, ['class' => 'btn btn-outline-info']) ?>
+            <?= Html::submitButton('Simpan', ['class' => 'btn btn-primary float-right']) ?>
+        </div>
 
-    <?= $form->field($model, 'nama_dosis')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'keterangan')->textarea(['rows' => 6]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
 
 </div>
